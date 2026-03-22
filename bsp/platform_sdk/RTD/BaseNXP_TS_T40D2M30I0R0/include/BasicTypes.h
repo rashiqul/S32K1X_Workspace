541c3db6ad90523b1f848824e1cb0806d79e8b86
Mohammad Rashiqul Alam <md.rashiqul.alam@gmail.com>
2026-03-22 16:00:54 -0400

    feat: bundle NXP SDK headers and add ARM CI build job
    - Copy NXP Platform SDK headers into bsp/platform_sdk/RTD/ (485 files)
      from BaseNXP_TS_T40D2M30I0R0/{header,include} and
      Platform_TS_T40D2M30I0R0/{include,startup/include} so the ARM
      firmware build is self-contained and requires no S32DS installation
    - Update bsp/CMakeLists.txt to prefer bundled headers over the S32DS
      install path, falling back gracefully when neither is available
    - Add arm-build CI job to linux-gcc-pipeline.yml, modelled on the
      S32K3x reference pipeline: installs gcc-arm-none-eabi, validates the
      s32k1x_arm_cortex_m4 Conan profile, runs make build_all_tgt, verifies
      ELF/HEX/BIN artifacts, validates ARM binary architecture, and uploads
      artifacts; arm-build added as required check in pipeline-summary

---
+++ bsp/platform_sdk/RTD/BaseNXP_TS_T40D2M30I0R0/include/BasicTypes.h
@@ -0,0 +1,28 @@
+/*==================================================================================================
+* Project : RTD AUTOSAR 4.7
+* Platform : CORTEXM
+* Peripheral : S32K14X
+* Dependencies : none
+*
+* Autosar Version : 4.7.0
+* Autosar Revision : ASR_REL_4_7_REV_0000
+* Autosar Conf.Variant :
+* SW Version : 3.0.0
+* Build Version : S32K1_RTD_3_0_0_QLP04_D2509_ASR_REL_4_7_REV_0000_20250930
+*
+* Copyright 2020-2025 NXP
+*
+* NXP Confidential and Proprietary. This software is owned or controlled by NXP and may only be 
+*   used strictly in accordance with the applicable license terms.  By expressly 
+*   accepting such terms or by downloading, installing, activating and/or otherwise 
+*   using the software, you are agreeing that you have read, and that you agree to 
+*   comply with and are bound by, such license terms.  If you do not agree to be 
+*   bound by the applicable license terms, then you may not retain, install,
+*   activate or otherwise use the software.
+==================================================================================================*/
+#ifndef BASICTYPES_H
+#define BASICTYPES_H
+
+#include "Platform_Types.h"
+
+#endif /* BASICTYPES_H */
---
+++ bsp/platform_sdk/RTD/BaseNXP_TS_T40D2M30I0R0/include/MemAcc_GeneralTypes.h
@@ -0,0 +1,104 @@
+/*==================================================================================================
+* Project : RTD AUTOSAR 4.7
+* Platform : CORTEXM
+* Peripheral : S32K14X
+* Dependencies : none
+*
+* Autosar Version : 4.7.0
+* Autosar Revision : ASR_REL_4_7_REV_0000
+* Autosar Conf.Variant :
+* SW Version : 3.0.0
+* Build Version : S32K1_RTD_3_0_0_QLP04_D2509_ASR_REL_4_7_REV_0000_20250930
+*
+* Copyright 2020-2025 NXP
+*
+* NXP Confidential and Proprietary. This software is owned or controlled by NXP and may only be 
+*   used strictly in accordance with the applicable license terms.  By expressly 
+*   accepting such terms or by downloading, installing, activating and/or otherwise 
+*   using the software, you are agreeing that you have read, and that you agree to 
+*   comply with and are bound by, such license terms.  If you do not agree to be 
+*   bound by the applicable license terms, then you may not retain, install,
+*   activate or otherwise use the software.
+==================================================================================================*/
+/**
+*   @file    MemAcc_GeneralTypes.h
+*
+*   @version 3.0.0
+*
+*   @brief   AUTOSAR BaseNXP - General Types.
+*   @details API header for all types and constants that are shared among the module mem.
+*            This file contains sample code only. It is not part of the production code deliverables
+*   @addtogroup BASENXP_COMPONENT
+*   @{
+*/
+
+#ifndef MEMACC_GENERAL_TYPES_H
+#define MEMACC_GENERAL_TYPES_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                              SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+
+/** 
+@{
+* @brief Parameters that shall be published within the modules header file.
+*       The integration of incompatible files shall be avoided.
+*/
+#define MEMACC_GENERALTYPES_AR_RELEASE_MAJOR_VERSION      4
+#define MEMACC_GENERALTYPES_AR_RELEASE_MINOR_VERSION      7
+
+/**@}*/
+/*==================================================================================================
+*                                     FILE VERSION CHECKS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                          CONSTANTS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                      DEFINES AND MACROS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                             ENUMS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                STRUCTURES AND OTHER TYPEDEFS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                GLOBAL VARIABLE DECLARATIONS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                    FUNCTION PROTOTYPES
+==================================================================================================*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*MEMACC_GENERAL_TYPES_H*/
+
+/** @} */
